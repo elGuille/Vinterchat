4e5a09968b89cf95bef285d8edd06ee53783955c
Guille <conandebarberian@gmail.com>
2017-01-17 20:29:33 +0000

    snaps in list

---
+++ Vinterchat/Snap.swift
@@ -0,0 +1,17 @@
+//
+//  Snap.swift
+//  Vinterchat
+//
+//  Created by Guillermo García on 17/01/2017.
+//  Copyright © 2017 Guillermo García. All rights reserved.
+//
+
+import Foundation
+
+
+class Snap {
+    
+    var imageURL = ""
+    var descrip = ""
+    var from = ""
+}
